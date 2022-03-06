124c11effd69924115618245e47f6db660f6fdba
puhaoran223 <haoran.pu@me2zen.com>
2022-03-06 23:17:39 +0800

    add memory

--- memory_dictionary.py
+++ memory_dictionary.py
@@ -4,6 +4,33 @@
 import os
 from random import choice
 
+mtl_dict = {
+    'ESSM': 0,
+    'MMOE': 0,
+    'PLE': 0,
+}
+
+dl_dict = {
+    '损失函数': 0,
+    '激活函数': 0,
+    '优化算法': 0,
+    '过/欠拟合': 0,
+    '梯度消失与梯度爆炸': 0,
+    'RNN': 0,
+    'GRU': 0,
+    'LSTM': 0,
+}
+
+ltr_dict = {
+    'LR': 0,
+    'GBDT': 0,
+    'FM': 0,
+    'NCF': 0,
+    'Wide&Deep': 0,
+    'DIN': 0,
+    'BST': 0,
+}
+
 memory_dict = {
     '快速排序.py': 1,
     '合并集合.py': 1,
@@ -17,6 +44,16 @@ memory_dict = {
 }
 
 
+def choice_kg(dict, item_num=1):
+    items = []
+    kg_list = []
+    for k, v in dict.items():
+        items += [k] * max(1, (5-v))
+    for _ in range(item_num):
+        kg_list.append(choice(items))
+    print(kg_list)
+
+
 def main():
     files = [i for i in os.listdir('./') if i.endswith('.py') and i != 'memory_dictionary.py']
     l = []
@@ -27,6 +64,10 @@ def main():
         num = max(1, num)
         l += [file] * num
     print(choice(l))
+
+    choice_kg(dl_dict, 2)
+    choice_kg(ltr_dict, 1)
+    choice_kg(mtl_dict, 1)
 
 
 main()
