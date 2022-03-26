177fa5e7732b2f1556841418f78720b7651e336b
puhaoran223 <haoran.pu@me2zen.com>
2022-03-27 00:05:42 +0800

    add hot

--- hot100/48. 旋转图像.py
+++ hot100/48. 旋转图像.py
@@ -26,4 +26,37 @@
 输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 """
+""" 题解
+先水平翻转，再反对角线翻转。
+1 2 3
+4 5 6
+7 8 9 
+=》先水平翻转
+3 2 1
+6 5 4
+9 8 7
+=》再反对角线翻转
+7 4 1
+8 5 2
+9 6 3
+"""
 
+
+class Solution:
+    def rotate(self, matrix) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            matrix[i] = matrix[i][::-1]
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n-1-i):
+                matrix[i][j], matrix[n-1-j][n-1-i] = matrix[n-1-j][n-1-i], matrix[i][j]
+
+
+matrix = [[1,2,3],
+[4,5,6],
+[7,8,9]]
+solution = Solution()
+solution.rotate(matrix)
