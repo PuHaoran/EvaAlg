36eeb0e4ec7060bf8cc5edf3ffc0d0950a95d08c
puhaoran223 <haoran.pu@me2zen.com>
2022-05-11 10:53:38 +0800

    add hot100

--- hot100/1. 两数之和.py
+++ hot100/1. 两数之和.py
@@ -25,9 +25,9 @@
 
 class Solution:
     def twoSum(self, nums, target: int):
-        num_dict = {}
+        d = {}
         for i in range(len(nums)):
-            if target-nums[i] not in num_dict:
-                num_dict[nums[i]] = i
+            if nums[i] in d:
+                return [d[nums[i]], i]
             else:
-                return [num_dict[target-nums[i]], i]
+                d[target-nums[i]] = i
